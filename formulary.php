e662f6ba8416c8351e04794d23bce02a35879cf6
ThomAzgo <weifang.buisson@gmail.com>
2021-12-07 16:12:56 +0100

    formulary

---
+++ formulary.php
@@ -0,0 +1,21 @@
+<?php
+    $BDD = array();
+    $BDD['host'] = "localhost";
+    $BDD['user'] = "root";
+    $BDD['pass'] = "";
+    $BDD['db'] = "jeanstore_projet";
+
+    $mysqli = mysqli_connect($BDD['host'],
+                            $BDD['pass'],
+                            $BDD['db']);
+    
+    if(!$mysqli) {
+        echo "Connexion échoué.";
+        exit;
+    }
+
+    $AfficherFormulaire = 1;
+
+    if (isset($_POST['pseudo'],$_POST['mdp']))
+    
+?>
