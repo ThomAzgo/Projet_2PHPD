025bbaa93418809ef739daaf046ed7f13cbfd1f6
ThomAzgo <weifang.buisson@gmail.com>
2021-12-07 10:27:16 +0100

    project

---
+++ private/shared/staff_footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <footer>
+            &copy; <?php echo date('Y'); ?> Jeans Store
+        </footer>
+    </body>
+</hmtl>
---
+++ public/staff/pages/index.php
@@ -0,0 +1,23 @@
+<?php
+    
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>JeansStore</title>
+</head>
+<body>
+    <header>
+        <h1>JeansStore Staff Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Menu</a></li>
+        </ul>
+    </navigation>
+    <footer>
+        &copy; 2021 JeansStore
+    </footer>
+</body>
+</html>
