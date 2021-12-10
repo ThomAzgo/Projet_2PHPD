a17342e163b2df35384fe0fdf28ae6885a435f22
ThomAzgo <weifang.buisson@gmail.com>
2021-12-10 16:06:09 +0100

    inscription

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+    $database = [
+            'host' => 'dbseverhost',
+            'database' => 'database',
+            'username' => 'username',
+            'password' => 'password',
+    ];
+
+    $dsn = "mysql:host={$database['host']};dbname={$database['database']};charset=utf8";
+    $link = new PDO($dsn, $database['username'], $database['password'], [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+?>
--- users/inscription.php
+++ users/inscription.php
@@ -1,35 +1,51 @@
 <?php
-    $BDD = array();
-    $BDD['host'] = "localhost";
-    $BDD['user'] = "root";
-    $BDD['pass'] = "";
-    $BDD['db'] = "jeanstore_projet";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "users";
 
-    $mysqli = mysqli_connect($BDD['host'],
-                            $BDD['pass'],
-                            $BDD['db']);
+        $name = $_POST["name"]; 
+        $email = $_POST["email"];
 
-    if(!$mysqli) {
-        echo "Connexion échoué.";
-        exit;
-    }
+        
+        if(!$mysqli) {
+            echo "Connexion failed.";
+            exit;
+        }
 
-    if (isset($_POST['pseudo'],$_POST['mdp'])) {
-        if(empty($_POST['pseudo'])) {
-            echo "Le champ Pseudo est vide.";
-    } elseif(!preg_match("#^[a-z0-9]+$#",$_POST['pseudo'])) {
-        echo "Le Pseudo doit être renseigné en lettres minuscules sans accents, sans caractères spéciaux.";
-    } elseif(strlen($_POST['pseudo'])>25) {
-        echo "Le pseudo est trop long, il dépasse 25 caractères.";
-    } elseif(empty($_POST['mdp'])){
-        echo "Le champ Mot de passe est vide.";
-    } elseif(mysqli_num_rows(mysqli_query($mysqli,"SELECT * FROM membres WHERE pseudo='".$_POST['pseudo']."'"))==1){
-        echo "Ce pseudo est déjà utilisé.";
-    } else {
-        if(!mysqli_query($mysqli,"INSERT INTO membres SET pseudo='".$_POST['pseudo']."', mdp='".md5($_POST['mdp'])."'")){
-            echo "Une erreur s'est produite: ".mysqli_error($mysqli);
+        if (isset($_POST['email'],$_POST['password'])) {
+            if(empty($_POST['username'])) {
+                echo "The username tag is empty.";
+        } elseif(!preg_match("#^[a-z0-9]+$#",$_POST['username'])) {
+            echo "The username should be in lowercase without spaces, numbers or special characters.";
+        } elseif(strlen($_POST['username'])>25) {
+            echo "The username is too long, it should be at 25 characters at max.";
+        } elseif(empty($_POST['password'])){
+            echo "The password tag is empty.";
+        } elseif(mysqli_num_rows(mysqli_query($mysqli,"SELECT * FROM users WHERE username='".$_POST['username']."'"))==1){
+            echo "This username is not available.";
         } else {
-            echo "Vous êtes inscrit avec succès!";
+            if(!mysqli_query($mysqli,"INSERT INTO users SET username='".$_POST['username']."', password='".md5($_POST['password'])."'")){
+                echo "An error as appeared: ".mysqli_error($mysqli);
+            } else {
+                echo "You've registered with success!";
+                }
+            }
+        }
+
+        $mysqli = new mysqli($host, $username, $password, $database);
+        if ($mysqli->connect_error) {
+            die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+        }
+        $statement = $mysqli->prepare("INSERT INTO users (name, email) VALUES(?, ?)"); 
+
+        $statement->bind_param('ss', $name, $email); 
+            
+        if($statement->execute()){
+            print "Salut " . $name . "!, votre adresse e-mail est ". $email;
+        }else{
+            print $mysqli->error;
         }
     }
 ?>
